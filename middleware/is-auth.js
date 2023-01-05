9265a4e35115647034e73d110194c727d41b0dc4
pranoy.ghosh <pranoyg52@gmail.com>
2023-01-05 15:47:40 +0530

    Added Redirection Logic for Pages

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -4,5 +4,14 @@ module.exports = (req, res, next) => {
   if (!users_id) {
     res.redirect("/");
   }
+  if (req.originalUrl.toUpperCase() == "/ONLOAD" && stored_session.users_id.userRole != "ADMIN") {
+    res.redirect("/");
+  }
+  if (req.originalUrl.toUpperCase() == "/OPENSECURITY" && stored_session.users_id.userRole != "SUPPORT") {
+    res.redirect("/");
+  }
+  if (req.originalUrl.toUpperCase() == "/DASHBOARD" && stored_session.users_id.userRole == "SUPPORT") {
+    res.redirect("/");
+  }
   next();
 };
